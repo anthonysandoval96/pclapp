081291fddebae3400f81f9aa039118c9436d8a5d
anthonysandoval <anthonysandoval1596@gmail.com>
2021-07-17 13:22:56 -0500

    Configuracion con credenciales de produccion izipay

--- app/libs/izipay/keys.php
+++ app/libs/izipay/keys.php
@@ -10,16 +10,16 @@ require_once __DIR__ . '/rest-php-sdk-master/src/autoload.php';
 
 /* Username, password and endpoint used for server to server web-service calls */
 //(En el Back Office) Copiar Usuario
-Lyra\Client::setDefaultUsername("55579682");
+Lyra\Client::setDefaultUsername("87558222");
 //(En el Back Office) Copiar Contraseña de test
-Lyra\Client::setDefaultPassword("testpassword_lJW13iYDE9nwmrJD7M7rGXmdRaZzvpoTRwRScsDQmewfR");
+Lyra\Client::setDefaultPassword("prodpassword_ITS5h1VWgtynSmHTPFOB0dWrsBRAVxYEOUE8dWhRiKJqB");
 //(En el Back Office) Copiar Contraseña de Nombre del servidor API REST
 Lyra\Client::setDefaultEndpoint("https://api.micuentaweb.pe");
 
 /* publicKey and used by the javascript client */
 //(En el Back Office) Copiar Clave pública de test
-Lyra\Client::setDefaultPublicKey("55579682:testpublickey_wcgEPYT7gxPzUJEJE6HfnmbG18Hj8mMPBd2JUumwu8Lkn");
+Lyra\Client::setDefaultPublicKey("87558222:publickey_R7akWFQiOYSyhPMzAJcY63Cs9eFaqaziBpfEG2VDVaRGI");
 
 /* SHA256 key */
 //(En el Back Office) Clave HMAC-SHA-256 de test
-Lyra\Client::setDefaultSHA256Key("Yga5AOlU5qomnyEj3EQvwMvpotybpd7q4Yk0z9ZZtUaJQ");
+Lyra\Client::setDefaultSHA256Key("m89AvrmX3UwOzC5jcGTT9qxHTYEpt9IXzne5NtMYNJiIE");
